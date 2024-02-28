9b6ec22c7d5eb5996a91647a310dc2152dfe7c24
Jarom Wilcox <wiljar00@users.noreply.github.com>
2024-02-27 22:48:51 -0700

    Add Python solution

---
+++ reverse_string/python/main.py
@@ -0,0 +1,8 @@
+def reverse_string(input_string):
+    return input_string[::-1]
+
+input_str = "Hello, World!"
+output_str = reverse_string(input_str)
+
+print(f"Input: {input_str}")
+print(f"Output: {output_str}")
