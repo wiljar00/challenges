2059965c0b806eac7ac609ab058cd6002d20f64f
Jarom Wilcox <wiljar00@users.noreply.github.com>
2024-02-27 22:49:25 -0700

    Ruby solution

---
+++ reverse_string/ruby/main.rb
@@ -0,0 +1,10 @@
+def reverse_string(input_str)
+    input_str.reverse
+  end
+  
+  input_str = "Hello, World!"
+  output_str = reverse_string(input_str)
+  
+  puts "Input: #{input_str}"
+  puts "Output: #{output_str}"
+  
