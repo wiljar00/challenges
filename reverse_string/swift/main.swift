8e0987a78cd84f3014f63009604e90285bf56542
Jarom Wilcox <wiljar00@users.noreply.github.com>
2024-02-27 22:59:17 -0700

    Solution in Swift

---
+++ reverse_string/swift/main.swift
@@ -0,0 +1,9 @@
+func reverseString(_ inputStr: String) -> String {
+    return String(inputStr.reversed())
+}
+
+let inputStr = "Hello, World!"
+let outputStr = reverseString(inputStr)
+
+print("Input: \(inputStr)")
+print("Output: \(outputStr)")
