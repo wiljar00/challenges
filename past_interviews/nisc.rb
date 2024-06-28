57710c20101c7387b04a07fee7d048c4902912ad
Jarom Wilcox <wiljar00@users.noreply.github.com>
2024-06-27 22:21:51 -0600

    Update nisc.rb

--- past_interviews/nisc.rb
+++ past_interviews/nisc.rb
@@ -1,4 +1,4 @@
-# interview session
+# NISC - Software Engineer II
 
 
 # Given an infinite $20, $10, $5, $2 and $1 bills. Write a program that will return the change for n dollars that would require the least amount of bills to be returned
@@ -41,5 +41,32 @@ end
 # input = 1000
 # get_bills(input)
 
+###############################################################################################
+###############################################################################################
+# feel really dumb... shoulda figured this one out or at least walked through with comments. 
 
+# Answer:
+def make_change(amount)
+    bills = [20, 10, 5, 2, 1]
+    change = {}
+  
+    bills.each do |bill|
+      if amount >= bill
+        count = amount / bill
+        change[bill] = count
+        amount %= bill
+      end
+    end
+  
+    change
+  end
+  
+  # Example usage:
+  amount = 47
+  change = make_change(amount)
+  puts "Change for $#{amount}:"
+  change.each do |bill, count|
+    puts "$#{bill} bill(s): #{count}"
+  end
 
+  make_change(1020)
