66c9c2ee7dd843cdcdc5ea9d6cb88cfca95d4e2e
Jarom Wilcox <wiljar00@users.noreply.github.com>
2024-07-22 21:59:12 -0600

    Add solution using loops

--- unique_substring/unique_substring.rb
+++ unique_substring/unique_substring.rb
@@ -73,3 +73,34 @@ input = "abcabcbb"
 length, substring = get_substring(input)
 puts "Length: #{length}"
 puts "Substring: #{substring}"
+
+# -----------------------
+
+def substring_using_loops(input)
+    char_index = {}
+    left = 0
+    max_length = 0
+    start = 0
+  
+    for right in 0...input.length
+      char = input[right]
+      if char_index.key?(char) && char_index[char] >= left
+        left = char_index[char] + 1
+      end
+
+      char_index[char] = right
+
+      if right - left + 1 > max_length
+        max_length = right - left + 1
+        start = left
+      end
+    end
+  
+    longest_substring = input[start, max_length]
+    return max_length, longest_substring
+  end
+
+input = "abcabcbb"
+length, substring = substring_using_loops(input)
+puts "Length: #{length}"
+puts "Substring: #{substring}"
