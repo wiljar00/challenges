b4d13e5c93cee1de44da7bc1574aa48e931dc950
Jarom Wilcox <wiljar00@users.noreply.github.com>
2024-07-22 21:51:44 -0600

    Add solution

--- unique_substring/unique_substring.rb
+++ unique_substring/unique_substring.rb
@@ -19,4 +19,27 @@
 # Input: s = "pwwkew"
 # Output: 3
 # Explanation: The answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
 # -----------------------
+# -----------------------
+
+# Solution
+
+def get_substring(input)
+    char_index = {}
+    left = 0
+    max_length = 0
+  
+    input.each_char.with_index do |char, right|
+      if char_index.key?(char) && char_index[char] >= left
+        left = char_index[char] + 1
+      end
+      char_index[char] = right
+      max_length = [max_length, right - left + 1].max
+    end
+  
+    max_length
+end
+
+input = "abcabcbb"
+puts get_substring(input)
