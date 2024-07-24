6737b403acef769f6dc7a311a46faba93e796dda
Jarom Wilcox <wiljar00@users.noreply.github.com>
2024-07-23 21:29:08 -0600

    Update diagonal_difference.rb

--- diagonal_difference/diagonal_difference.rb
+++ diagonal_difference/diagonal_difference.rb
@@ -7,15 +7,7 @@
 # The function accepts 2D_INTEGER_ARRAY arr as parameter.
 
 def diagonalDifference(array)
-    puts "output: #{array}"
-
     # 1. find the sum of the first diagonal starting at 1 x 1
-    first_row = array[0]
-    first_value = first_row[0]
-
-    matrix_size = array.length
-    last_row = array[matrix_size - 1]
-
     first_sum = 0
     second_sum = 0
 
@@ -23,27 +15,21 @@ def diagonalDifference(array)
         number = value[index]
         first_sum += number
     end
-
-    puts first_sum
     
     # 2. find the sum of the second diagonal, starting at 1 x array[length - 1]
-
     array.reverse.each_with_index do |value, index|
         number = value[index]
         second_sum += number
     end
-    
-    puts second_sum
 
-    # 3. find out which one is greater
-
+    # 3. find out which one is greater and subtract
     if first_sum >= second_sum
         result = first_sum - second_sum
     else
         result = second_sum - first_sum
     end
 
-    # 4. subtract and return the difference. 
+    # 4. return the difference. 
     result
 end
 
