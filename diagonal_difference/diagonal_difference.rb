2c7bb7ea16214ed8e0d23e1c4fc46823b7797f5b
Jarom Wilcox <wiljar00@users.noreply.github.com>
2024-07-23 21:08:24 -0600

    Setup question

---
+++ diagonal_difference/diagonal_difference.rb
@@ -0,0 +1,19 @@
+
+# Complete the 'diagonalDifference' function below.
+#
+# Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+
+def diagonalDifference(array)
+    puts "output: #{array}"
+end
+
+array = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+diagonalDifference(array)
