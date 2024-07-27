3bb849fb50c27d8b6e31560e86a78494ebf6aa90
Jarom Wilcox <wiljar00@users.noreply.github.com>
2024-07-26 20:42:23 -0600

    Update staircase.rb

---
+++ staircase/staircase.rb
@@ -0,0 +1,19 @@
+
+# This is a staircase of size :
+   #
+  ##
+ ###
+####
+# Its base and height are both equal to n. It is drawn using # symbols and spaces. The last line is not preceded by any spaces.
+# Write a program that prints a staircase of size .
+
+# Complete the 'staircase' function below.
+#
+# The function accepts INTEGER n as parameter.
+#
+
+def staircase(n)
+    # Write your code here
+    
+
+end
