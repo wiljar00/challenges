d5d32f9d411987a43c24f8af2946c098504a1855
Jarom Wilcox <wiljar00@users.noreply.github.com>
2024-07-26 20:55:40 -0600

    first solution

--- staircase/staircase.rb
+++ staircase/staircase.rb
@@ -1,10 +1,10 @@
-
 # This is a staircase of size :
    #
   ##
  ###
 ####
 # Its base and height are both equal to n. It is drawn using # symbols and spaces. The last line is not preceded by any spaces.
+
 # Write a program that prints a staircase of size .
 
 # Complete the 'staircase' function below.
@@ -12,8 +12,21 @@
 # The function accepts INTEGER n as parameter.
 #
 
+def line_printer(n)
+    string = ""
+
+    for i in 1..n
+        string = string << "#"
+    end
+
+    puts string
+end
+
 def staircase(n)
     # Write your code here
-    
-
+    for i in 1..n
+        line_printer(i)
+    end
 end
+
+staircase(4)
