49e7a137bbf95333098e6da3925cfcb59b602f5b
Jarom Wilcox <wiljar00@users.noreply.github.com>
2024-07-27 20:07:24 -0600

    First solution using sort

--- min_max_sum/min_max_sum.rb
+++ min_max_sum/min_max_sum.rb
@@ -7,16 +7,30 @@
 # The function accepts INTEGER_ARRAY arr as parameter.
 #
 
-def miniMaxSum(arr)
-    # Write your code here
+def first_answer_using_sort(arr)
+    sorted_array = arr.sort
+    array_length = sorted_array.length
 
-    min = 0
-    max = 0
+    min = sorted_array[0] + sorted_array[1] + sorted_array[2] + sorted_array[3]
+    max = sorted_array[array_length-1] + sorted_array[array_length - 2] + sorted_array[array_length - 3] + sorted_array[array_length - 4]
 
     puts "#{min} #{max}"
 end
 
-array = [1, 3, 5, 7, 9]
+def miniMaxSum(arr)
+    # Write your code here
+    min = 0
+    max = 0
+
+    # first idea - using sort:
+    # 1. sort the array from small to large
+    # 2. min is the first two array values added together
+    # 3. max is the last two array values added together
+    first_answer_using_sort(arr)
+end
+
+# array = [1, 3, 5, 7, 9]
+array = [1, 3, 7, 9, 5]
 # expected result: 
 # 16, 24
 miniMaxSum(array)
