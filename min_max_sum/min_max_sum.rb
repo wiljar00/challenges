ff51737541d52fd0234b0034985ae3117b640f87
Jarom Wilcox <wiljar00@users.noreply.github.com>
2024-07-27 19:56:53 -0600

    Add question

---
+++ min_max_sum/min_max_sum.rb
@@ -0,0 +1,22 @@
+# Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly 
+#   four of the five integers. Then print the respective minimum and maximum values as a single line of two 
+#   space-separated long integers.
+
+# Complete the 'miniMaxSum' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def miniMaxSum(arr)
+    # Write your code here
+
+    min = 0
+    max = 0
+
+    puts "#{min} #{max}"
+end
+
+array = [1, 3, 5, 7, 9]
+# expected result: 
+# 16, 24
+miniMaxSum(array)
