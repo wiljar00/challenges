2a4fe45805e33fb97803d331e929e79c7401c224
Jarom Wilcox <wiljar00@users.noreply.github.com>
2024-07-28 21:21:46 -0600

    Add birthday cake candle question

---
+++ cake_candles/cake_candles.rb
@@ -0,0 +1,20 @@
+# You are in charge of the cake for a child's birthday. 
+# You have decided the cake will have one candle for each year of their total age. 
+# They will only be able to blow out the tallest of the candles. 
+# Count how many candles are tallest.
+
+# Complete the 'birthdayCakeCandles' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY candles as parameter.
+#
+
+def birthdayCakeCandles(candles)
+    # Write your code here
+    candles = 0
+
+    puts "#{candles}"
+end
+
+candles = [4, 4, 1, 3]
+birthdayCakeCandles(candles)
