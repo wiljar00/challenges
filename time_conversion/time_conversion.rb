4eb064d2e8a468c9dafb3e1f49648c85331f1152
Jarom Wilcox <wiljar00@users.noreply.github.com>
2024-07-29 21:29:10 -0600

    Add first solution

--- time_conversion/time_conversion.rb
+++ time_conversion/time_conversion.rb
@@ -8,9 +8,33 @@
 # The function accepts STRING s as parameter.
 #
 
+def first_solution(s)
+    hour = s[0..1]
+    minute = s[3..4]
+    second = s[6..7]
+    am_pm = s[8..9]
+
+    if am_pm == 'PM'
+        hour = hour.to_i + 12
+        if hour.to_i == 24
+            hour = "12"
+        end
+    end
+
+    if am_pm == 'AM'
+        if hour.to_i == 12
+            hour = "00"
+        end
+    end
+
+    "#{hour}:#{minute}:#{second}"
+end
+
 def timeConversion(s)
     # Write your code here
 
+    time = first_solution(s)
+    time
 end
 
 time = "12:01:00PM"
@@ -18,3 +42,6 @@ puts timeConversion(time) # expected result: '12:01:00'
 
 time2 = "12:01:00AM"
 puts timeConversion(time2) # expected result: '00:01:00'
+
+time3 = "07:05:45PM"
+puts timeConversion(time3) # expected result: '19:05:45'
