64c883550e34ccc193e6bc685511b09b6eeb833d
Jarom Wilcox <wiljar00@users.noreply.github.com>
2024-07-29 22:08:11 -0600

    Add solution

--- grading_students/grading_students.rb
+++ grading_students/grading_students.rb
@@ -1,6 +1,6 @@
 # HackerLand University has the following grading policy:
 #  - Every student receives a grade in the inclusive range from 0 to 100.
-#  - Any grade less than  is a failing grade.
+#  - Any grade less than 40 is a failing grade.
 # Sam is a professor at the university and likes to round each student's grade according to these rules:
 #  - If the difference between the grade and the next multiple of 5 is less than 3, round grade up to the next multiple of 5.
 #  - If the value of grade is less than 38, no rounding occurs as the result will still be a failing grade.
@@ -20,13 +20,24 @@
 
 def gradingStudents(grades)
     # grades is an integer array
-    result = grades
+    result = []
+
+    grades.each do |grade|
+      if grade < 38
+        result << grade
+      else
+        next_multiple_of_5 = ((grade / 5) + 1) * 5
+        if next_multiple_of_5 - grade < 3
+          result << next_multiple_of_5
+        else
+          result << grade
+        end
+      end
+    end
 
     # returns integer array
     result 
 end
 
 grades = [73, 67, 38, 33]
-
-puts gradingStudents(grades)
-# expect: [75, 67, 40, 33]
+puts gradingStudents(grades) # expect: [75, 67, 40, 33]
