7678cc703425ec2308da91235a2f42b79b992ae8
Jarom Wilcox <wiljar00@users.noreply.github.com>
2024-08-02 22:50:29 -0600

    Update find_factors.rb

---
+++ findFactors/find_factors.rb
@@ -0,0 +1,24 @@
+# There will be two arrays of integers. Determine all integers that satisfy the following two
+# conditions:
+# 1. The elements of the first array are all factors of the integer being considered
+# 2. The integer being considered is a factor of all elements of the second array
+# These numbers are referred to as being between the two arrays. Determine how many such numbers exist.
+
+# Complete the 'getTotalX' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY a
+#  2. INTEGER_ARRAY b
+#
+
+def getTotalX(a, b)
+    # Write your code here
+
+end
+
+
+a = [2, 6]
+b = [24, 36]
+
+puts getTotalX(a, b)
