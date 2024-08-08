a9fac2fccc0522f2d0ce6a940f55e2a3baebb523
Jarom Wilcox <wiljar00@users.noreply.github.com>
2024-08-07 22:31:13 -0600

    Update divisible_sum_pairs.rb

---
+++ divisible_sum_pairs/divisible_sum_pairs.rb
@@ -0,0 +1,23 @@
+# Given an array of integers and a positive integer k, 
+# determine the number of (i, j) pairs where i < j and arli] + ar[i] is divisible by k.
+
+#
+# Complete the 'divisibleSumPairs' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER k
+#  3. INTEGER_ARRAY ar
+#
+
+def divisibleSumPairs(n, k, ar)
+    # Write your code here
+    ar
+end
+
+ar = [1, 2, 3, 4, 5, 6]
+k = 5
+n = ar.length
+
+puts divisibleSumPairs(n, k, ar)
