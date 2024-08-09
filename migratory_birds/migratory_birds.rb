d39d65389786a40b3bc12d06ab038592dde5f234
Jarom Wilcox <wiljar00@users.noreply.github.com>
2024-08-08 22:44:34 -0600

    Update migratory_birds.rb

---
+++ migratory_birds/migratory_birds.rb
@@ -0,0 +1,20 @@
+# Given an array of bird sightings where every element represents a bird type id, 
+# determine the id of the most frequently sighted type. If more than 1 type has been spotted that maximum amount, 
+# return the smallest of their ids.
+
+
+#
+# Complete the 'migratoryBirds' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def migratoryBirds(arr)
+    # Write your code here
+    
+end
+
+arr = [1, 4, 4, 4, 5, 3]
+
+puts migratoryBirds(arr) # expected result: 4
