47521ce177cd6da22d1a05c77e969c27c520b5c7
Jarom Wilcox <wiljar00@users.noreply.github.com>
2024-08-09 23:49:44 -0600

    Update even_sums.rb

---
+++ even_sums/even_sums.rb
@@ -0,0 +1,17 @@
+# Challenge: Sum of Even Numbers
+# Problem:
+# Write a function in Ruby that takes an array of integers as input and 
+# returns the sum of all even numbers in the array.
+
+def sum_of_evens(arr)
+    "test"
+end
+
+puts sum_of_evens([1, 2, 3, 4, 5, 6])
+# => 12
+
+puts sum_of_evens([7, 8, 9, 10])
+# => 18
+
+puts sum_of_evens([11, 13, 15])
+# => 0
