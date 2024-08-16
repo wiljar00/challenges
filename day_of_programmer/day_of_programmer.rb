6c9ae40306ba4dd0da50774b3333830a467bfdaf
Jarom Wilcox <wiljar00@users.noreply.github.com>
2024-08-15 22:10:21 -0600

    Update day_of_programmer.rb

---
+++ day_of_programmer/day_of_programmer.rb
@@ -0,0 +1,21 @@
+
+
+#
+# Complete the 'dayOfProgrammer' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts INTEGER year as parameter.
+#
+
+def dayOfProgrammer(year)
+    # Write your code here
+    year = 'year'
+    # do stuff
+    year
+end
+
+year1 = 2016
+puts dayOfProgrammer(year1)
+
+year2 = 1800
+puts dayOfProgrammer(year2)
