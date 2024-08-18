c1294e1830849da536d2f7955c0a79e720e1e58b
Jarom Wilcox <wiljar00@users.noreply.github.com>
2024-08-17 22:17:27 -0600

    Update page_count.rb

---
+++ page_count/page_count.rb
@@ -0,0 +1,26 @@
+# A teacher asks the class to open their books to a page number. 
+# A student can either start turning pages from the front of the book or from the back of the book. 
+# They always turn pages one at a time. When they open the book, page 1 is always on the right side:
+# When they flip page 1, they see pages 2 and 3. Each page except the last page will always be printed on both sides. 
+# The last page may only be printed on the front, given the length of the book. 
+# If the book is n pages long, and a student wants to turn to page p, what is the minimum number of pages to turn? 
+# They can start at the beginning or the end of the book.
+# Given n and p, find and print the minimum number of pages that must be turned in order to arrive at page
+#
+# Complete the 'pageCount' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n - number of pages in the book
+#  2. INTEGER p - page number to turn to
+#  3. returns: int - the minimum number of pages to turn
+#
+
+def pageCount(n, p)
+    # Write your code here
+
+end
+
+n = 1
+p = 1
+puts pageCount(n, p)
