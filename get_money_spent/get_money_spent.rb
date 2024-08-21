f2286f3e1ac1302bda0d67e854ff0ef8c2f9db45
Jarom Wilcox <wiljar00@users.noreply.github.com>
2024-08-20 21:43:49 -0600

    Create get_money_spent.rb

---
+++ get_money_spent/get_money_spent.rb
@@ -0,0 +1,25 @@
+# A person wants to determine the most expensive computer keyboard and USB drive that can be purchased with a give budget.
+# Given price lists for keyboards and USB drives and a budget, find the cost to buy them. 
+# If it is not possible to buy both items, return -1.
+
+#
+# Complete the getMoneySpent function below.
+#
+def getMoneySpent(keyboards, drives, b)
+    #
+    # Write your code here.
+    #
+    'test'
+end
+
+keyboards = [3, 1]
+drives = [5, 2, 8]
+b = 10
+
+puts getMoneySpent(keyboards, drives, b) # expects 9
+
+keyboards = [5]
+drives = [4]
+b = 5
+
+puts getMoneySpent(keyboards, drives, b) # expects -1
