ceee39a1394f61fdd5352801cc8213cfea1da0be
Jarom Wilcox <wiljar00@users.noreply.github.com>
2024-08-22 22:54:30 -0600

    Create magic_squares.rb

---
+++ magic_squares/magic_squares.rb
@@ -0,0 +1,11 @@
+#
+# Complete the 'formingMagicSquare' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY s as parameter.
+#
+
+def formingMagicSquare(s)
+  # Write your code here
+
+end
