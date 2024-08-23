a2802e4b0be74e743744622a178777bbf0522c7e
Jarom Wilcox <wiljar00@users.noreply.github.com>
2024-08-22 22:40:58 -0600

    Update cat_and_mouse.rb

--- cat_and_mouse/cat_and_mouse.rb
+++ cat_and_mouse/cat_and_mouse.rb
@@ -1,4 +1,35 @@
+# Two cats and a mouse are at various positions on a line. 
+# You will be given their starting positions. 
+# Your task is to determine which cat will reach the mouse first, 
+# assuming the mouse does not move and the cats travel at equal speed. 
+# If the cats arrive at the same time, the mouse will be allowed to move and it will escape while they fight.
+
+# You are given q queries in the form of x, y, and z representing the respective positions for cats A and B,
+# and for mouse C. Complete the function catAndMouse to return the appropriate answer to each query, 
+# which will be printed on a new line.
+
+
+# Input:
+# x = Cat A's position
+# y = Cat B's position
+# z = Mosue C's position
+# return value = String ('Cat A', 'Cat B', or 'Mouse C')
+
 def catAndMouse(x, y, z)
-
-
+    'test'
 end
+
+x = 2
+y = 5
+z = 4
+puts catAndMouse(x, y, z) # expects 'Cat B'
+
+x = 1
+y = 2
+z = 3
+puts catAndMouse(x, y, z) # expects 'Cat B'
+
+x = 1
+y = 3
+z = 2
+puts catAndMouse(x, y, z) # expects 'Mouse C'
