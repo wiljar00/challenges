cdd864ac77e817ca6c881e3bb14bedb3581e9055
Jarom Wilcox <wiljar00@users.noreply.github.com>
2024-08-30 00:03:42 -0600

    Update subarray.rb

---
+++ subarray/subarray.rb
@@ -0,0 +1,11 @@
+#
+# Complete the 'pickingNumbers' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def pickingNumbers(a)
+    # Write your code here
+
+end
