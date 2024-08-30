7653e616f3a6f44b01a3a697475cc0957a158d11
Jarom Wilcox <wiljar00@users.noreply.github.com>
2024-08-30 00:07:51 -0600

    Update subarray.rb

--- subarray/subarray.rb
+++ subarray/subarray.rb
@@ -1,3 +1,5 @@
+# Given an array of integers, find the longest subarray where the absolute difference between any two elements is less than or equal to 1.
+
 #
 # Complete the 'pickingNumbers' function below.
 #
@@ -7,5 +9,17 @@
 
 def pickingNumbers(a)
     # Write your code here
+    subarray = a[0]
+    
 
+    subarray
 end
+
+a = [1, 1, 2, 2, 4, 4, 5, 5, 5]
+puts pickingNumbers(a) # expects to return 5
+
+a = [4, 6, 5, 3, 3, 1]
+puts pickingNumbers(a) # expects to return 5
+
+a = [1, 2, 2, 3, 1, 2]
+puts pickingNumbers(a) # expects to return 5
