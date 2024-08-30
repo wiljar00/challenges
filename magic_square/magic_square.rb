c56115fee39f47ff1cac2ff5e6a02ad149519bc6
Jarom Wilcox <wiljar00@users.noreply.github.com>
2024-08-29 23:53:20 -0600

    Update magic_square.rb

--- magic_square/magic_square.rb
+++ magic_square/magic_square.rb
@@ -1,3 +1,11 @@
+# We define a magic square to be an n x n matrix of distinct positive integers from 1 to n^2 
+# where the sum of any row, column, or diagonal of length n is always equal to the same
+# number: the magic constant.
+# You will be given a 3 x 3 matrix s of integers in the inclusive range [1, 9]. 
+# We can convert any digit a to any other digit b in the range [1, 9] at cost of la - b. 
+# Given s, convert it into a magic square at minimal cost. Print this cost on a new line.
+# Note: The resulting magic square must contain distinct integers in the inclusive range [1, 9].
+
 #
 # Complete the 'formingMagicSquare' function below.
 #
@@ -7,5 +15,10 @@
 
 def formingMagicSquare(s)
     # Write your code here
+    result = 0
 
+    return result
 end
+
+s = [[5, 3, 4], [1, 5, 8], [6, 4, 2]]
+puts formingMagicSquare(s) # Expected result: 7
