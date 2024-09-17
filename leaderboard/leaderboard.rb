5e6f81465c9ab6b3c518db1b4eedae78b480e114
Jarom Wilcox <wiljar00@users.noreply.github.com>
2024-09-17 12:00:25 -0600

    Add examples to problem

--- leaderboard/leaderboard.rb
+++ leaderboard/leaderboard.rb
@@ -9,10 +9,11 @@
 # ranked = [100, 90, 90, 80]
 # player = [70, 80, 105]
 
-# The ranked players will have ranks 1, 2, 2, and 3, respectively. If the player's scores are 70, 80 and 105, their rankings after each game are 4th, gra and 18t. Return [4, 3, 1].
+# The ranked players will have ranks 1, 2, 2, and 3, respectively. If the player's scores are 70, 80 and 105, 
+# their rankings after each game are 4th, 3rd and 1st. Return [4, 3, 1].
+
 # Function Description
 
-# Complete the climbing Leaderboard function in the editor below.
 # climbingLeaderboard has the following parameters):
 # • int rankedIn: the leaderboard scores
 # • int playerm!: the player's scores
@@ -33,3 +34,9 @@ def climbingLeaderboard(ranked, player)
     # Write your code here
 
 end
+
+
+ranked = [100, 90, 90, 80]
+player = [70, 80, 105]
+
+climbingLeaderboard(ranked, player) # should return [4, 3, 1]
