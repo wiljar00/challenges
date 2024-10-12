09e3737d98c10e1493f969af55ef1adae4a84e23
Jarom Wilcox <wiljar00@users.noreply.github.com>
2024-10-11 20:54:22 -0600

    Add final substring solution and test cases.

--- substring/substring.rb
+++ substring/substring.rb
@@ -4,13 +4,7 @@ def ruby_cheat(full_string, substring)
   full_string.include?(substring) ? 'yes' : 'no'
 end
 
-def find_substring(full_string:, substring:)
-  result = 'no'
-
-  # first solution - ruby cheat code solution
-  # result = ruby_cheat(full_string, substring)
-
-  # second solution - figure it out myself
+def my_solution(full_string, substring)
   for i in 0..(full_string.length - substring.length)
     current_slice = full_string[i, substring.length]
 
@@ -19,11 +13,25 @@ def find_substring(full_string:, substring:)
       break
     end
   end
+end
+
+def find_substring(full_string:, substring:)
+  result = 'no'
+
+  # first solution - ruby cheat code solution
+  # result = ruby_cheat(full_string, substring)
+
+  # second solution - figure it out myself
+  my_solution(full_string, substring)
 
   result
 end
 
-full_string = 'testing'
-substring = 'test'
-
-puts find_substring(full_string: full_string, substring: substring)
+# Test cases
+puts "Full string: 'testing', Substring: 'test' => #{find_substring(full_string: 'testing', substring: 'test')}     # Expected: yes (substring at start)"
+puts "Full string: 'testing', Substring: 'ing' => #{find_substring(full_string: 'testing', substring: 'ing')}      # Expected: yes (substring at end)"
+puts "Full string: 'testing', Substring: 'sti' => #{find_substring(full_string: 'testing', substring: 'sti')}      # Expected: yes (substring in the middle)"
+puts "Full string: 'testing', Substring: 'hello' => #{find_substring(full_string: 'testing', substring: 'hello')}     # Expected: no (substring not present)"
+puts "Full string: 'testing', Substring: 'testing' => #{find_substring(full_string: 'testing', substring: 'testing')}  # Expected: yes (full string and substring are identical)"
+puts "Full string: 'testing', Substring: '' => #{find_substring(full_string: 'testing', substring: '')}         # Expected: yes (empty substring is technically 'found' at every position)"
+puts "Full string: '', Substring: 'test' => #{find_substring(full_string: '', substring: 'test')}             # Expected: no (empty full string)"
