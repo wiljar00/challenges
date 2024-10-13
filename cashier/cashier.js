36dfa4cbe4956e842e62f559f9f4d41623d80644
Jarom Wilcox <wiljar00@users.noreply.github.com>
2024-10-12 23:34:28 -0600

    Update cashier.js

--- cashier/cashier.js
+++ cashier/cashier.js
@@ -1,5 +1,15 @@
-function cashier() {
-    console.log("test");
+// Given a total number amount, return the smallest number of bills adding up to the total. 
+// Bills are given in 20, 10, 5, 1
+
+
+function cashier(bills, amount) {
+    console.log(`Bills: ${bills}`)
+    console.log(`Amount: ${amount}`)
 }
 
-cashier();
+
+let bills = [20, 10, 5, 1];
+let amount = 125;
+
+
+cashier(bills, amount);
