42ff7e192bfb1066f3fc225daf01d901bca43077
Jarom Wilcox <wiljar00@users.noreply.github.com>
2024-10-14 23:14:00 -0600

    Update new_stack.js

--- new_stack/new_stack.js
+++ new_stack/new_stack.js
@@ -1,1 +1,19 @@
-console.log("new_stack")
+// Challenge: 
+// Implement the stack using a class.
+// optional - Ensure each method operates with constant time complexity (O(1)).
+
+
+class myStack {
+
+}
+
+
+
+// Usage: 
+const stack = new myStack();
+stack.push(10);
+stack.push(20);
+console.log(stack.peek()); // 20
+console.log(stack.pop());  // 20
+console.log(stack.isEmpty()); // false
+console.log(stack.size()); // 1
