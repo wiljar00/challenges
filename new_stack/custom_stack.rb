2e9a5ea81903f7aaadcc36e4ec1336da2c2f0746
Jarom Wilcox <wiljar00@users.noreply.github.com>
2024-10-18 21:58:33 -0600

    CustomStack - add push method

--- new_stack/custom_stack.rb
+++ new_stack/custom_stack.rb
@@ -6,12 +6,23 @@ class CustomStack
   def print_stack
     puts @stack.to_s
   end
+
+  def push(value)
+    # add validations to value type
+    @stack.push(value)
+  end
 end
 
 
 # testing class
 # initialize
-customStack = CustomStack.new
+custom_stack = CustomStack.new
 
 # print stack
-customStack.print_stack
+custom_stack.print_stack # expect [1, 3, 5]
+
+# add values to stack
+custom_stack.push(1)
+custom_stack.push(3)
+custom_stack.push(5)
+custom_stack.print_stack # expect [1, 3, 5]
