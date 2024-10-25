8851b8acd183cfbbade5e5bc1feedb5eb98e6da4
Jarom Wilcox <wiljar00@users.noreply.github.com>
2024-10-24 22:57:33 -0600

    Update first_non_repeating_character.js

--- first_non_repeating_character/first_non_repeating_character.js
+++ first_non_repeating_character/first_non_repeating_character.js
@@ -13,7 +13,22 @@
  */
 
 function firstNonRepeatingCharacter(str) {
-  // Implement your solution here
+  const charCount = {};
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    if (charCount[char]) {
+      charCount[char]++;
+    } else {
+      charCount[char] = 1;
+    }
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    if (charCount[str[i]] === 1) {
+      return str[i];
+    }
+  }
+  return null;
 }
 
 // Test cases
