6d01b3c18086294f3f5938f6ba95f52dadc0ae98
Jarom Wilcox <wiljar00@users.noreply.github.com>
2024-10-25 21:53:17 -0600

    Create get_money_spent.js

---
+++ get_money_spent/get_money_spent.js
@@ -0,0 +1,28 @@
+/**
+ * A person wants to determine the most expensive computer keyboard and USB drive that can be purchased with a given budget.
+ * Given price lists for keyboards and USB drives and a budget, find the cost to buy them. 
+ * If it is not possible to buy both items, return -1.
+ */
+
+/**
+ * Complete the getMoneySpent function below.
+ */
+function getMoneySpent(keyboards, drives, b) {
+
+}
+
+// Test cases
+let keyboards = [40, 50, 60];
+let drives = [5, 8, 12];
+let b = 60;
+console.log(getMoneySpent(keyboards, drives, b)); // expects 58
+
+keyboards = [3, 1];
+drives = [5, 2, 8];
+b = 10;
+console.log(getMoneySpent(keyboards, drives, b)); // expects 9
+
+keyboards = [5];
+drives = [4];
+b = 5;
+console.log(getMoneySpent(keyboards, drives, b)); // expects -1
