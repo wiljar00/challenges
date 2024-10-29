93eddc2af316fce068c69752c0509f4d3fe83fef
Jarom Wilcox <wiljar00@users.noreply.github.com>
2024-10-28 23:49:02 -0600

    Update first_non_repeating_character.rb

---
+++ first_non_repeating_character/first_non_repeating_character.rb
@@ -0,0 +1,21 @@
+def first_non_repeating_character(str)
+  char_map = Hash.new(0)
+  
+  # Count occurrences of each character
+  str.each_char do |char|
+    char_map[char] += 1
+  end
+  
+  # Find first character with count of 1
+  str.each_char do |char|
+    return char if char_map[char] == 1
+  end
+  
+  'nothing'
+end
+
+# Test cases
+puts first_non_repeating_character("aabccd")    # Should output: "b"
+puts first_non_repeating_character("aabbcc")    # Should output: "nothing"
+puts first_non_repeating_character("abcdef")    # Should output: "a"
+puts first_non_repeating_character("aabbccdeeff")  # Should output: "d"
