b86d19cfaa611c2593c64fd1925cf728d9bc3451
Jarom Wilcox <wiljar00@users.noreply.github.com>
2024-10-31 23:13:48 -0600

    Update fifo.rb

--- queues/fifo/fifo.rb
+++ queues/fifo/fifo.rb
@@ -4,11 +4,32 @@ class FifoQueue
   def initialize
     @queue = []
   end
+
+  def add(item)
+    @queue.push(item)
+  end
+
+  def remove
+    @queue.shift
+  end
+
+  def print_queue
+    puts "queue: #{@queue}"
+  end
 end
 
 fifo_queue = FifoQueue.new
+puts "adding 1"
 fifo_queue.add(1)
+fifo_queue.print_queue
+puts "adding 2"
 fifo_queue.add(2)
+puts "adding 3"
 fifo_queue.add(3)
 
+puts "removing a value, expects 1, got: #{fifo_queue.remove}"
+fifo_queue.print_queue 
+puts "removing a value, expects 2, got: #{fifo_queue.remove}"
+fifo_queue.print_queue
+puts "removing a value, expects 3, got: #{fifo_queue.remove}"
 fifo_queue.print_queue
