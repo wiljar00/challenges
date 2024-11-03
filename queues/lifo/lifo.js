5c1e3cfa170158530a48abff8b8dece1ae464c35
Jarom Wilcox <wiljar00@users.noreply.github.com>
2024-11-02 23:04:11 -0600

    Update lifo.js

--- queues/lifo/lifo.js
+++ queues/lifo/lifo.js
@@ -12,12 +12,23 @@ class LifoQueue {
   remove() {
     return this.queue.pop();
   }
+
+  printQueue() {
+    console.log(this.queue);
+  }
 }
 
 const lifoQueue = new LifoQueue();
 lifoQueue.add(1);
+console.log("queue should be [1]");
+lifoQueue.printQueue();
 lifoQueue.add(2);
+console.log("queue should be [1, 2]");
+lifoQueue.printQueue();
 lifoQueue.add(3);
-console.log(lifoQueue.queue);
+console.log("queue should be [1, 2, 3]");
+lifoQueue.printQueue();
+console.log("remove should be 3");
 console.log(lifoQueue.remove());
+console.log("queue should be [1, 2]");
 console.log(lifoQueue.queue);
