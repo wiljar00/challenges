02e4ed5247c2ab961af0cd5badb3bc87c91fd376
Jarom Wilcox <wiljar00@users.noreply.github.com>
2024-11-03 23:30:33 -0700

    Add FIFO queue in javascript

---
+++ queues/fifo/fifo.js
@@ -0,0 +1,36 @@
+// create a fifo queue in javascript
+
+
+class FifoQueue {
+    constructor() {
+      this.queue = [];
+    }
+  
+    add(item) {
+      this.queue.push(item);
+    }
+  
+    remove() {
+      return this.queue.pop();
+    }
+  
+    printQueue() {
+      console.log(this.queue);
+    }
+  }
+  
+  const lifoQueue = new LifoQueue();
+  lifoQueue.add(1);
+  console.log("queue should be [1]");
+  lifoQueue.printQueue();
+  lifoQueue.add(2);
+  console.log("queue should be [1, 2]");
+  lifoQueue.printQueue();
+  lifoQueue.add(3);
+  console.log("queue should be [1, 2, 3]");
+  lifoQueue.printQueue();
+  console.log("remove should be 3");
+  console.log(lifoQueue.remove());
+  console.log("queue should be [1, 2]");
+  console.log(lifoQueue.queue);
+  
