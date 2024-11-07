3d02983fc611447ade8d2709b6e14b2eed7f7089
Jarom Wilcox <wiljar00@users.noreply.github.com>
2024-11-07 16:42:25 -0700

    Update random.js

--- random_stuff/js/random.js
+++ random_stuff/js/random.js
@@ -9,13 +9,30 @@
 // /todos
 // /users
 
-const mainEndpoint = "https://jsonplaceholder.typicode.com/";
-
-function get_data() {
-  fetch(mainEndpoint + "todos/1")
-    .then((response) => response.json())
-    .then((json) => console.log(json));
+async function getData(url) {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    throw new Error("Error with the request: " + error.message);
+  }
 }
 
-console.log("Data returned: ");
-get_data();
+function processData(data) {
+  console.log("Processing data:");
+  console.log(`Title: ${data.title}`);
+}
+
+const url = "https://jsonplaceholder.typicode.com/todos/1";
+getData(url)
+  .then((data) => {
+    console.log("Data returned:", data);
+    processData(data); // Pass the fetched data to the processing function
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+  });
