f217de8820adbbc29f0f4ed0da59770a81f95d2c
Jarom Wilcox <wiljar00@users.noreply.github.com>
2024-12-16 21:33:05 -0700

    todo list - add basic list functionality

--- todo_list/frontend_only/JavascriptTodoList/script.js
+++ todo_list/frontend_only/JavascriptTodoList/script.js
@@ -1,5 +1,29 @@
+const todoList = [];
+
 function showPopup() {
-  alert("testing alert");
+  alert(todoList);
 }
 
-document.getElementById("testBtn").addEventListener("click", showPopup);
+function refreshList() {
+  var taskList = document.getElementById("taskList");
+  taskList.innerHTML = "";
+
+  todoList.forEach(function (task) {
+    var listItem = document.createElement("li");
+    listItem.textContent = task;
+    taskList.appendChild(listItem);
+  });
+}
+
+function addTask() {
+  var description = document.getElementById("taskInput").value;
+  if (description) {
+    todoList.push(description);
+    refreshList();
+    document.getElementById("taskInput").value = "";
+  } else {
+    alert("Please enter a task.");
+  }
+}
+
+document.getElementById("addTaskBtn").addEventListener("click", addTask);
